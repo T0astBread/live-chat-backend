b4124223a4e80c7b533efe8507ed4f716658b797
T0astBread <tb@t0ast.cc>
2021-02-05 21:40:15 +0100

    Add CORS headers to allow requests from any origin
    In production this would be narrowed down to only include necessary
    origins

--- server.go
+++ server.go
@@ -13,6 +13,26 @@ import (
 
 const defaultPort = "8080"
 
+type corsWrapper struct {
+	underlying *handler.Server
+}
+
+// ServeHTTP wraps the underlying handler.Server and adds CORS
+// headers
+func (c *corsWrapper) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods", "POST")
+		rw.Header().Set("Access-Control-Allow-Headers", "*")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+
+	c.underlying.ServeHTTP(rw, req)
+}
+
 //go:generate gqlgen generate
 func main() {
 	port := os.Getenv("PORT")
@@ -23,7 +43,7 @@ func main() {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", &corsWrapper{srv})
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
